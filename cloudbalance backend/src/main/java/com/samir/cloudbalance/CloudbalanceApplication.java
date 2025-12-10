aa5341be96a5bcbcbcde4450e5348517cab5a546
Sameer-khann <sameerkhann241@gmail.com>
2025-12-10 10:57:45 +0530

    Working on FE

---
+++ cloudbalance backend/src/main/java/com/samir/cloudbalance/CloudbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.samir.cloudbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbalanceApplication.class, args);
+	}
+
+}
